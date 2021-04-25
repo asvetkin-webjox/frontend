664407c5227f7a57bec0d61d2ff8e4e86a6c6ce0
33and2 <sevpor26@gmail.com>
2021-04-25 18:33:00 -0500

    fix: typo

--- src/components/UI/Input/InputStyle.js
+++ src/components/UI/Input/InputStyle.js
@@ -24,32 +24,3 @@ export const InputStyle = (
     alignItems: 'center',
   },
 });
-
-// const { ...rest } = InputStyle();
-
-// export const InputIndexStyle = (
-//   { palette: { secondary, blueLight } },
-//   { width = '100%', isAdornment = true },
-// ) => ({
-//   input: {
-//     width,
-//     padding: isAdornment ? '11px 15px 11px 44px' : '10px 15px',
-//     color: secondary.main,
-//     borderRadius: '4px',
-//     border: `1px solid ${blueLight}`,
-//     '&::placeholder': {
-//       color: secondary.main,
-//       opacity: '1',
-//       transform: 'translateY(1px)',
-//       fontSize: '1.2rem',
-//     },
-//   },
-//   notchedOutline: {
-//     border: 'none',
-//   },
-//   root: {
-//     width,
-//     height: '40px',
-//     alignItems: 'center',
-//   },
-// });
